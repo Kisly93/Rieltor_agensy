bd4f77ad1d59940688dbe8a139bcc7a690300c15
Kisly93 <spsvk.kis.sov@gmail.com>
2023-05-22 21:07:58 +0200

    addede @register

--- property/admin.py
+++ property/admin.py
@@ -7,7 +7,7 @@ class RelationInInline(admin.TabularInline):
     model = Owner.owner_apartment.through
     raw_id_fields = ('owner',)
 
-
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address']
     readonly_fields = ['created_at']
@@ -17,17 +17,15 @@ class FlatAdmin(admin.ModelAdmin):
     raw_id_fields = ('likes',)
     inlines = [RelationInInline]
 
-
+@admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ('apartment',)
 
-
+@admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     search_fields = ['owner_name']
     raw_id_fields = ('owner_apartment',)
     list_display = ('owner_name',)
 
 
-admin.site.register(Flat, FlatAdmin)
-admin.site.register(Complaint, ComplaintAdmin)
-admin.site.register(Owner, OwnerAdmin)
+
